b545584cc96b95e404bea8ba324025e499cce2d6
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-30 18:47:28 +0800

    feat: Add CreateScreen and + icon on indexscreen header

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import { createStackNavigator} from "react-navigation-stack";
 import { createAppContainer } from 'react-navigation'
 import { Provider as BlogProvider } from "./src/context/BlogContext";
 import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
   Show: ShowScreen,
+  Create: CreateScreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
