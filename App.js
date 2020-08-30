510415b71e1ddf6c832313f2b63414aba0f79a94
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-30 18:35:29 +0800

    feat: Add ShowScreen and onPress for blog items

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import IndexScreen from "./src/screens/indexScreen";
 import { createStackNavigator} from "react-navigation-stack";
 import { createAppContainer } from 'react-navigation'
 import { Provider as BlogProvider } from "./src/context/BlogContext";
+import ShowScreen from "./src/screens/ShowScreen";
 
 const navigator = createStackNavigator({
-  Index: IndexScreen
+  Index: IndexScreen,
+  Show: ShowScreen,
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
